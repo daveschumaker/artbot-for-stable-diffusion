7cfa16613e2fc4576959f7f20b4f9cadca7a892b
Dave Schumaker <daveschumaker@users.noreply.github.com>
2022-10-15 06:56:49 -0700

    chore: add try cath around telemetry api call

--- api/telemetry.ts
+++ api/telemetry.ts
@@ -1,9 +1,13 @@
-export const trackEvent = (obj = {}) => {
-  fetch(`/artbot/api/telemetry`, {
-    method: 'POST',
-    body: JSON.stringify(obj),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
+export const trackEvent = async (obj = {}) => {
+  try {
+    await fetch(`/artbot/api/telemetry`, {
+      method: 'POST',
+      body: JSON.stringify(obj),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  } catch (err) {
+    // If nothing happens, it's fine to ignore this.
+  }
 }
