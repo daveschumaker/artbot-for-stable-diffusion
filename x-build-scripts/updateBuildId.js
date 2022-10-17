d38145af17b4e08345b7807f38c21928675d5a39
Dave Schumaker <daveschumaker@users.noreply.github.com>
2022-10-17 08:38:05 -0700

    chore: fix build scripts

--- x-build-scripts/updateBuildId.js
+++ x-build-scripts/updateBuildId.js
@@ -1,26 +1,20 @@
 const fs = require('fs')
 const buildFile = './build_info.json'
 
-try {
-  if (fs.existsSync(buildFile)) {
-    const date = new Date()
-    const buildArr = [
-      date.getFullYear(),
-      date.getMonth(),
-      '_',
-      date.getHours(),
-      date.getMinutes(),
-      date.getSeconds()
-    ]
+const date = new Date()
+const buildArr = [
+  date.getFullYear(),
+  date.getMonth(),
+  '_',
+  date.getHours(),
+  date.getMinutes(),
+  date.getSeconds()
+]
 
-    const buildInfo = {
-      build: buildArr.join('')
-    }
-
-    const data = JSON.stringify(buildInfo)
-
-    fs.writeFileSync(buildFile, data)
-  }
-} catch (err) {
-  console.error(err)
+const buildInfo = {
+  build: buildArr.join('')
 }
+
+const data = JSON.stringify(buildInfo)
+
+fs.writeFileSync(buildFile, data)
