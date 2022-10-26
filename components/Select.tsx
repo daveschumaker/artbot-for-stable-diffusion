fb4341c6bbc1575cfe93e8eeece0cb837a2f3fc8
Dave Schumaker <daveschumaker@users.noreply.github.com>
2022-10-26 10:40:23 -0700

    chore: stash work and add react-select

--- components/Select.tsx
+++ components/Select.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
+import Select from 'react-select'
 import styled from 'styled-components'
-
 interface SelectProps {
   name?: string
   children?: React.ReactNode
@@ -9,20 +9,30 @@ interface SelectProps {
   value?: string
 }
 
-const StyledSelect = styled.select<SelectProps>`
-  background-color: rgb(42, 48, 60);
-  border-radius: 4px;
-  border: 1px solid white;
-  color: #e1e1e1;
-  font-size: 14px;
-  height: 39px;
-  padding: 8px;
+const StyledSelect = styled(Select)<SelectProps>`
+  & .Select__control {
+    border-color: black;
+  }
+
+  position: relative;
+
+  /* background-color: rgb(42, 48, 60); */
+  /* border-radius: 4px; */
+  /* border: 1px solid white; */
+  /* color: #e1e1e1; */
+  /* font-size: 14px; */
+  /* height: 39px; */
+  /* padding: 8px; */
   width: ${(props) => (props.width ? props.width : '120px')};
 `
 
-const Select = (props: SelectProps) => {
+const SelectComponent = (props: SelectProps) => {
   const { children, ...rest } = props
-  return <StyledSelect {...rest}>{children}</StyledSelect>
+  return (
+    <StyledSelect classNamePrefix={'Select'} {...rest}>
+      {children}
+    </StyledSelect>
+  )
 }
 
-export default Select
+export default SelectComponent
