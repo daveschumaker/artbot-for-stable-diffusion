543b67179b8ac19bc534a687c0f0fc843e42ed78
Dave Schumaker <daveschumaker@users.noreply.github.com>
2022-10-28 14:58:39 -0700

    feat: implement painting page

---
+++ pages/paint.tsx
@@ -0,0 +1,14 @@
+// import styled from 'styled-components'
+import PageTitle from '../components/UI/PageTitle'
+import PaintCanvas from '../components/PaintCanvas'
+
+const PaintPage = () => {
+  return (
+    <div>
+      <PageTitle>Paint</PageTitle>
+      <PaintCanvas />
+    </div>
+  )
+}
+
+export default PaintPage
