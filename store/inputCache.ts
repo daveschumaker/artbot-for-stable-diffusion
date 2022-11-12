d57f35d30465912119da2217d0a90feba539251a
Dave Schumaker <daveschumaker@users.noreply.github.com>
2022-11-12 11:26:38 -0800

    chore: initial work on simple input cache to auto save state when user leaves page

---
+++ store/inputCache.ts
@@ -0,0 +1,18 @@
+/**
+ * Store all details related to input cache so that we can quickly restore if a user
+ * inadvertently moves from the page.
+ */
+
+let inputCache: any = null
+
+export const clearInputCache = () => {
+  inputCache = null
+}
+
+export const getInputCache = () => {
+  return Object.assign({}, inputCache)
+}
+
+export const setInputCache = (input: any) => {
+  inputCache = Object.assign({}, input)
+}
