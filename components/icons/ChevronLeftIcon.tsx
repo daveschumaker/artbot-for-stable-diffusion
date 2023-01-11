7880fbedce9facbc2d274e472a5455ec9e0be8f5
Dave Schumaker <daveschumaker@users.noreply.github.com>
2023-01-10 16:35:04 -0800

    chore: add new icon

---
+++ components/icons/ChevronLeftIcon.tsx
@@ -0,0 +1,33 @@
+// Modified via: https://tabler-icons.io/
+
+const ChevronLeftIcon = ({
+  className,
+  fill = 'none',
+  size = 24,
+  stroke = 'currentColor'
+}: {
+  className?: string
+  fill?: string
+  size?: number
+  stroke?: string
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke={stroke}
+      fill={fill}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <polyline points="15 6 9 12 15 18"></polyline>
+    </svg>
+  )
+}
+
+export default ChevronLeftIcon
