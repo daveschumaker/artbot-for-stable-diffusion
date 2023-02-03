643b0190b62a2a6e1555435cb6b3f59bfceb4c65
Dave Schumaker <daveschumaker@users.noreply.github.com>
2023-02-03 13:35:18 -0800

    fix: wrong key

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -198,7 +198,7 @@ function MyApp({ Component, darkMode, pageProps }: MyAppProps) {
         <Footer />
         <div className="fixed right-[16px] bottom-[8px] hidden adCol:block max-w-[156px]">
           <AdContainer
-            code="CVAIKKQM"
+            code="CWYD62QI"
             placement="tinybotsnet"
             key={router.asPath}
             minSize={1440}
