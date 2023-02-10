1909e023af4ade88c80af6c46cd65aed7e98e34e
Dave Schumaker <daveschumaker@users.noreply.github.com>
2023-02-09 22:17:21 -0800

    fix: issue with invariant on ssr

--- components/CreatePage/AdvancedOptionsPanel/index.tsx
+++ components/CreatePage/AdvancedOptionsPanel/index.tsx
@@ -1,6 +1,8 @@
 import dynamic from 'next/dynamic'
 
-const DynamicOptionsPanel = dynamic(() => import('./advancedOptionsPanel'))
+const DynamicOptionsPanel = dynamic(() => import('./advancedOptionsPanel'), {
+  ssr: false
+})
 
 export default function AdvancedOptionsPanel(props: any) {
   return <DynamicOptionsPanel {...props} />
