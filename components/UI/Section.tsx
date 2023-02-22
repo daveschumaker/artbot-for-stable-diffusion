04eed3603e156480fe7df4dbcfa2e599d756fac2
Dave Schumaker <daveschumaker@users.noreply.github.com>
2023-02-22 00:00:44 -0800

    chore: remove unused component

--- components/UI/Section.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Section = ({ children }: { children?: React.ReactNode }) => {
-  return <div className="pt-[16px] first:pt-[0]">{children}</div>
-}
-
-export default Section
