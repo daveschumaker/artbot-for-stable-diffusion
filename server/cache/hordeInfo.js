240c3e4762e140bf1a75a2a70aa78d17fe3a6809
Dave Schumaker <daveschumaker@users.noreply.github.com>
2023-03-07 10:38:40 -0800

    chore: hacky workaround to ignore stale perf data from API

--- server/cache/hordeInfo.js
+++ server/cache/hordeInfo.js
@@ -6,6 +6,14 @@ const fetchHordePerformance = async () => {
   try {
     const res = await fetch('https://stablehorde.net/api/v2/status/performance')
     const data = await res.json()
+
+    // Temporary hack to get around an issue where the Stable Horde backend alternates
+    // between real request count and this weird / stale "43" queued_requests response.
+    // TODO: Remove when backend is fixed.
+    if (data?.queued_requests === 43) {
+      return
+    }
+
     perf = Object.assign({}, data)
   } catch (err) {}
 }
