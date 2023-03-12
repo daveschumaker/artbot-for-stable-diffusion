dfc332eb1e77600831e41dbe3967dba0ab6e4881
Dave Schumaker <daveschumaker@users.noreply.github.com>
2023-03-11 18:23:02 -0800

    chore: make PromptInputSettings a bit more robust

--- models/PromptInputSettings.ts
+++ models/PromptInputSettings.ts
@@ -1,6 +1,46 @@
 import LocalStorageController from '../controllers/LocalStorageController'
 
-const PromptInputSettings = new LocalStorageController({
+class PromptInputSettingsClass extends LocalStorageController {
+  constructor({ name, version }: { name: string; version: number }) {
+    super({ name, version })
+  }
+
+  saveAllInput(input: object) {
+    const clonedInput = Object.assign({}, input)
+
+    // Clone to prompt input settings
+    const keysToExclude = [
+      'canvasStore',
+      'canvasData',
+      'copyPrompt',
+      'maskData',
+      'source_image',
+      'source_mask'
+    ]
+
+    interface IKeyValue {
+      [key: string]: string
+    }
+
+    const keysToRemap: IKeyValue = {
+      orientation: 'orientationType'
+    }
+
+    for (let [key, value] of Object.entries(clonedInput)) {
+      if (keysToExclude.indexOf(key) >= 0) {
+        continue
+      }
+
+      if (keysToRemap[key]) {
+        key = keysToRemap[key]
+      }
+
+      this.set(key, value)
+    }
+  }
+}
+
+const PromptInputSettings = new PromptInputSettingsClass({
   name: 'PromptInputSettings',
   version: 1
 })
