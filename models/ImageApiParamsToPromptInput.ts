91434578aad7ac47e6c53eb236164849395ac9dd
Dave Schumaker <daveschumaker@users.noreply.github.com>
2023-03-12 09:48:45 -0700

    chore: class to convert api params shape back to image input shape

---
+++ models/ImageApiParamsToPromptInput.ts
@@ -0,0 +1,54 @@
+import DefaultPromptInput from './DefaultPromptInput'
+import { IApiParams } from './ImageParamsForApi'
+
+class ImageApiParamsToPromptInput {
+  cfg_scale: number
+  denoising_strength?: number
+  control_type?: string
+  height: number
+  karras: boolean
+  hires: boolean
+  clipskip: number
+  models: Array<string>
+  negative?: string
+  orientationType: string
+  parentJobId?: string
+  post_processing?: Array<string>
+  prompt: string
+  sampler: string
+  seed: string
+  source_image?: string
+  source_mask?: string
+  steps: number
+  tiling: boolean
+  triggers?: string[]
+  width: number
+  canvasData: any
+  maskData: any
+
+  constructor(imageParams: IApiParams) {
+    const defaultState = new DefaultPromptInput()
+
+    for (const [key, value] of Object.entries(defaultState)) {
+      // @ts-ignore
+      this[key] = value
+    }
+
+    this.prompt = imageParams.prompt
+    this.models = imageParams.models
+    this.cfg_scale = imageParams.params.cfg_scale
+    this.clipskip = imageParams.params.clip_skip
+    this.orientationType = 'custom'
+    this.height = imageParams.params.height
+    this.width = imageParams.params.width
+    this.hires = imageParams.params.hires_fix
+    this.karras = imageParams.params.karras
+    this.post_processing = imageParams.params.post_processing
+    this.sampler = imageParams.params.sampler_name
+    this.seed = imageParams.params.seed || ''
+    this.steps = imageParams.params.steps
+    this.tiling = imageParams.params.tiling
+  }
+}
+
+export default ImageApiParamsToPromptInput
