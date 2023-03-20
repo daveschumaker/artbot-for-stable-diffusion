0b0e044017a131d45c2b6e93ecadeeb829bd7757
Eugene Savchenko <savchenko2002je@gmail.com>
2023-03-20 09:56:46 +0300

    feat: added dimension swap button

--- components/CreatePage/AdvancedOptionsPanel/OrientationOptions/index.tsx
+++ components/CreatePage/AdvancedOptionsPanel/OrientationOptions/index.tsx
@@ -12,6 +12,9 @@ import NumericInputSlider from '../NumericInputSlider'
 import DefaultPromptInput from 'models/DefaultPromptInput'
 import { orientationDetails } from 'utils/imageUtils'
 
+import { Button } from 'components/UI/Button'
+import RefreshIcon from 'components/icons/RefreshIcon' // TODO: replace me with a more appropriate icon
+
 const orientationOptions = [
   { value: 'landscape-16x9', label: 'Landscape (16 x 9)' },
   { value: 'landscape', label: 'Landscape (3 x 2)' },
@@ -81,43 +84,60 @@ const OrientationOptions = ({ input, setInput, setErrorMessage }: Props) => {
                 )}
             </div>
 
-            <Section>
-              <NumericInputSlider
-                label="Width"
-                from={64}
-                to={
-                  loggedIn === true
-                    ? MAX_DIMENSIONS_LOGGED_IN
-                    : MAX_DIMENSIONS_LOGGED_OUT
-                }
-                step={64}
-                input={input}
-                setInput={setInput}
-                fieldName="width"
-                initialLoad={false}
-                fullWidth
-                enforceStepValue
-              />
-            </Section>
+            <div className="flex flex-column">
+              <div className="w-11/12">
+                <Section>
+                  <NumericInputSlider
+                    label="Width"
+                    from={64}
+                    to={
+                      loggedIn === true
+                        ? MAX_DIMENSIONS_LOGGED_IN
+                        : MAX_DIMENSIONS_LOGGED_OUT
+                    }
+                    step={64}
+                    input={input}
+                    setInput={setInput}
+                    fieldName="width"
+                    initialLoad={false}
+                    fullWidth
+                    enforceStepValue
+                  />
+                </Section>
 
-            <Section>
-              <NumericInputSlider
-                label="Height"
-                from={64}
-                to={
-                  loggedIn === true
-                    ? MAX_DIMENSIONS_LOGGED_IN
-                    : MAX_DIMENSIONS_LOGGED_OUT
-                }
-                step={64}
-                input={input}
-                setInput={setInput}
-                fieldName="height"
-                initialLoad={false}
-                fullWidth
-                enforceStepValue
-              />
-            </Section>
+                <Section>
+                  <NumericInputSlider
+                    label="Height"
+                    from={64}
+                    to={
+                      loggedIn === true
+                        ? MAX_DIMENSIONS_LOGGED_IN
+                        : MAX_DIMENSIONS_LOGGED_OUT
+                    }
+                    step={64}
+                    input={input}
+                    setInput={setInput}
+                    fieldName="height"
+                    initialLoad={false}
+                    fullWidth
+                    enforceStepValue
+                  />
+                </Section>
+              </div>
+              <div className="w-1/12 flex justify-center items-center">
+                <Button
+                title="Swap dimensions"
+                onClick={() => {
+                  setInput({
+                    height: input.width,
+                    width: input.height
+                  })
+                }}
+              >
+                <RefreshIcon />
+              </Button>
+              </div>
+            </div>
 
             <div className="block text-xs mt-2 w-full">
               Height and width must be divisible by 64. Enter your desired
