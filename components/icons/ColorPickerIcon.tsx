a2e6ca8e30ae8ff8a0af377ae1a8063f49dad944
Dave Schumaker <daveschumaker@users.noreply.github.com>
2023-04-13 11:04:30 -0700

    chore: update color picker icon

--- components/icons/ColorPickerIcon.tsx
+++ components/icons/ColorPickerIcon.tsx
@@ -1,8 +1,6 @@
-// Modified via: https://tabler-icons.io/
+// Modified via: https://www.svgrepo.com/svg/371788/color-picker
 
 const ColorPickerIcon = ({
-  className,
-  fill = 'none',
   size = 24,
   stroke = 'currentColor'
 }: {
@@ -13,20 +11,23 @@ const ColorPickerIcon = ({
 }) => {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
+      fill={stroke}
       width={size}
       height={size}
-      viewBox="0 0 24 24"
-      strokeWidth="1"
-      stroke={stroke}
-      fill={fill}
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      viewBox="0 0 36 36"
+      version="1.1"
+      preserveAspectRatio="xMidYMid meet"
+      xmlns="http://www.w3.org/2000/svg"
     >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M11 7l6 6"></path>
-      <path d="M4 16l11.7 -11.7a1 1 0 0 1 1.4 0l2.6 2.6a1 1 0 0 1 0 1.4l-11.7 11.7h-4v-4z"></path>
+      <path
+        d="M33.73,2.11a4.09,4.09,0,0,0-5.76.1L22.81,7.38a3.13,3.13,0,0,1-4.3.11L17.09,8.91,27,18.79l1.42-1.42A3.18,3.18,0,0,1,28.46,13l5.17-5.17A4.08,4.08,0,0,0,33.73,2.11Z"
+        className="clr-i-solid clr-i-solid-path-1"
+      ></path>
+      <path
+        d="M22.18,16.79,7.46,31.51a2,2,0,1,1-2.82-2.83L19.35,14l-1.41-1.41L3.22,27.27a4,4,0,0,0-.68,4.8L1.06,33.55a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l1.44-1.44a3.93,3.93,0,0,0,2.09.6,4.06,4.06,0,0,0,2.88-1.2L23.6,18.21Z"
+        className="clr-i-solid clr-i-solid-path-2"
+      ></path>
+      <rect x="0" y="0" width="36" height="36" fillOpacity="0" />
     </svg>
   )
 }
