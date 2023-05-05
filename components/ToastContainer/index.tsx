f512021157e1686c1f153f24148b71da328fa4c2
Dave Schumaker <daveschumaker@users.noreply.github.com>
2023-05-05 10:15:46 -0700

    fix: issue with next 13.4 and react-toastify

---
+++ components/ToastContainer/index.tsx
@@ -0,0 +1,15 @@
+// Handle isse with newer versions of NextJS (v13.4+)
+// Via: https://github.com/fkhadra/react-toastify/issues/951
+
+import dynamic from 'next/dynamic'
+
+const DynamicToastContainer = dynamic(
+  () => import('react-toastify').then((module) => module.ToastContainer),
+  {
+    ssr: false
+  }
+)
+
+const ToastContainer = (props: any) => <DynamicToastContainer {...props} />
+
+export default ToastContainer
