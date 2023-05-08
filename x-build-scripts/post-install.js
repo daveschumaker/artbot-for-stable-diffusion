3c6fe713d157bc23dae74e28eeaa9ddc0c206b8e
Dave Schumaker <daveschumaker@users.noreply.github.com>
2023-05-08 08:56:41 -0700

    fix: prevent .env from being overwritten

--- x-build-scripts/post-install.js
+++ x-build-scripts/post-install.js
@@ -1,8 +1,9 @@
-const fs = require('fs')
+// const fs = require('fs')
 
+// TODO: Disable as this seems t be overwriting the .env file
 // Check if .env file exists, otherwise, create it.
-if (!fs.existsSync('./env')) {
-  console.log(`\n\n-== Running ArtBot Post-Install Script==--`)
-  console.log(`> creating .env file`)
-  fs.writeFile('./.env', '', () => {})
-}
+// if (!fs.existsSync('./env')) {
+//   console.log(`\n\n-== Running ArtBot Post-Install Script==--`)
+//   console.log(`> creating .env file`)
+//   fs.writeFile('./.env', '', () => {})
+// }
