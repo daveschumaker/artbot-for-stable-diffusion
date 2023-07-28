0735bd99fb93733c45de0371a93dde1b56c4da0a
Dave Schumaker <daveschumaker@users.noreply.github.com>
2023-07-28 09:44:25 -0700

    chore: remove unneeded code

--- components/CreatePage/TriggerDropdown/index.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const DynamicTriggerDropdown = dynamic(() => import('./triggerDropdown'), {
-  ssr: false
-})
-
-export default function TriggerDropdown(props: any) {
-  return <DynamicTriggerDropdown {...props} />
-}
