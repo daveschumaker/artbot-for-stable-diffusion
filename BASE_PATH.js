424b9ecbbcb3ba98cfd98e2389cf58d0fbe269b7
Dave Schumaker <daveschumaker@users.noreply.github.com>
2023-07-31 17:13:09 -0700

    fix: base path

--- BASE_PATH.js
+++ BASE_PATH.js
@@ -1,5 +1,5 @@
 // const baseHost = 'https://tinybots.net'
-const baseHost = 'https://3bba-23-93-98-252.ngrok-free.app'
+const baseHost = 'http://staging.tinybots.net'
 const basePath = '/artbot'
 
 module.exports = {
