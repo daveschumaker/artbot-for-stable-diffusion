747e4187bb6c3d07fdfa00e38330221ad866f21b
Dave Schumaker <daveschumaker@users.noreply.github.com>
2023-09-26 08:48:34 -0700

    chore: fix linting issue on pre-commit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,13 +1,4 @@
 module.exports = {
-  // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
-
-  // Lint & Prettify TS and JS files
-  '**/*.(ts|tsx|js)': (filenames) => [
-    `npx eslint ${filenames.join(' ')}`,
-    `npx prettier --write ${filenames.join(' ')}`
-  ],
-
-  // Prettify only Markdown and JSON files
-  '**/*.(md|json)': (filenames) => `npx prettier --write ${filenames.join(' ')}`
+  '*.{ts,tsx,js}': ['eslint --fix', 'prettier --write'],
+  '*.{md,json}': 'prettier --write'
 }
