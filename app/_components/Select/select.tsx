ae7e6cf84598045d96ab5aeee31c5ac522c7b9ef
Dave Schumaker <daveschumaker@users.noreply.github.com>
2024-01-05 11:39:12 -0800

    stash work

--- app/_components/Select/select.tsx
+++ app/_components/Select/select.tsx
@@ -18,69 +18,96 @@ const SelectComponent = (props: SelectComponentProps) => {
     control: (provided: CSSProperties) => ({
       ...provided,
       backgroundColor: props.isDisabled ? '#adadad' : `var(--input-background)`,
-      borderColor: `var(--input-text)`,
-      color: `var(--input-text)`,
-      fontSize: '16px',
-      width: '100%',
-      minHeight: 'unset',
-      zIndex: `var(--zIndex-default)`,
-      height: '40px',
+      // borderColor: `var(--input-text)`,
+      // color: `var(--input-text)`,
+      // fontSize: '16px',
+      // width: '100%',
+      // minHeight: 'unset',
+      // zIndex: `var(--zIndex-default)`,
       '@media (min-width: 800px)': {
+        alignItems: 'flex-start',
         fontSize: '14px',
-        height: '30px',
-        lineHeight: '28px'
+        lineHeight: '28px',
+        minHeight: 'unset'
+        // height: '30px'
       }
     }),
-    input: () => ({
+    input: (provided: CSSProperties) => ({
+      ...provided,
       color: `var(--input-text)`,
-      height: '40px',
-      lineHeight: '40px',
-      margin: 0,
-      position: 'absolute',
-      top: 0,
-      left: '8px',
-      right: '8px',
-      bottom: 0,
+      // height: '40px',
+      // lineHeight: '40px',
+      // margin: 0,
+      // position: 'absolute',
+      // top: 0,
+      // left: '8px',
+      // right: '8px',
+      // bottom: 0,
       '@media (min-width: 800px)': {
         fontSize: '14px',
-        height: '28px',
-        lineHeight: '28px'
+        lineHeight: '30px',
+        margin: '0 2px',
+        minHeight: 'unset',
+        height: '30px',
+        padding: '0 2px'
       }
     }),
     option: (provided: CSSProperties, state: OptionProps) => ({
-      backgroundColor: state.isFocused ? '#7e5a6c' : `var(--input-background)`,
-      color: `var(--input-text)`,
-      borderRadius: '4px',
-      padding: '0 4px',
-      cursor: 'pointer',
-      marginBottom: '4px'
+      ...provided,
+      backgroundColor: state.isFocused ? '#7e5a6c' : `var(--input-background)`
+      // color: `var(--input-text)`,
+      // borderRadius: '4px',
+      // padding: '0 4px',
+      // cursor: 'pointer',
+      // marginBottom: '4px'
     }),
-    placeholder: () => ({
+    placeholder: (provided: CSSProperties) => ({
+      ...provided,
       color: '#9299a6'
     }),
-    clearIndicator: () => ({
-      height: 'calc(var(--input-element-h) - 2px)',
-      '@media (min-width: 800px)': {
-        height: '30px',
-        lineHeight: '28px'
-      }
+    clearIndicator: (provided: CSSProperties) => ({
+      ...provided
+      // height: 'calc(var(--input-element-h) - 2px)',
+      // '@media (min-width: 800px)': {
+      //   height: '30px',
+      //   lineHeight: '28px'
+      // }
     }),
-    dropdownIndicator: () => ({
-      color: `var(--input-text)`,
-      height: 'calc(var(--input-element-h) - 2px)',
-      '@media (min-width: 800px)': {
-        height: '30px',
-        lineHeight: '28px'
-      }
+    dropdownIndicator: (provided: CSSProperties) => ({
+      ...provided
+      // color: `var(--input-text)`,
+      // height: 'calc(var(--input-element-h) - 2px)',
+      // '@media (min-width: 800px)': {
+      //   height: '30px',
+      //   lineHeight: '28px'
+      // }
     }),
-    singleValue: () => ({
+    singleValue: (provided: CSSProperties) => ({
+      ...provided,
       color: `var(--input-text)`,
-      height: '38px',
-      lineHeight: '38px',
+      // height: '38px',
+      // lineHeight: '38px',
       '@media (min-width: 800px)': {
+        // position: 'relative',
+        // top: 0,
         fontSize: '14px',
-        height: '28px',
-        lineHeight: '28px'
+        lineHeight: '28px',
+        minHeight: 'unset',
+        height: '28px'
+      }
+    }),
+    multiValue: (provided: CSSProperties) => ({
+      ...provided,
+      backgroundColor: 'var(--main-color)',
+      '@media (min-width: 800px)': {
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'row',
+        fontSize: '14px',
+        lineHeight: '20px',
+        margin: '5px 2px 0 2px',
+        minHeight: 'unset',
+        height: '20px'
       }
     }),
     // multiValueContainer: () => ({
@@ -96,36 +123,40 @@ const SelectComponent = (props: SelectComponentProps) => {
     // }),
     valueContainer: (provided: CSSProperties) => ({
       ...provided,
-      color: `var(--input-text)`,
-      columnGap: '4px',
-      display: 'flex',
-      flexDirection: 'row',
-      height: '40px',
-      padding: '0 8px',
+      // color: `var(--input-text)`,
+      // columnGap: '4px',
+      // display: 'flex',
+      // flexDirection: 'row',
+      // height: '40px',
+      // padding: '0 8px',
       '@media (min-width: 800px)': {
-        fontSize: '14px',
-        height: '28px',
-        position: 'absolute',
-        lineHeight: '28px',
-        right: '24px',
-        left: 0,
-        top: 0,
-        bottom: 0
+        //   fontSize: '14px',
+        alignItems: 'flex-start',
+        minHeight: 'unset',
+        // height: '30px',
+        padding: '0 8px'
+        //   position: 'absolute',
+        //   lineHeight: '28px',
+        //   right: '24px',
+        //   left: 0,
+        //   top: 0,
+        //   bottom: 0
       }
     }),
     menu: (provided: CSSProperties) => ({
       ...provided,
-      backgroundColor: `var(--input-background)`,
-      color: `var(--input-text)`,
-      marginBottom: '16px',
-      zIndex: `var(--zIndex-overBase)`,
-      padding: '0 4px'
+      backgroundColor: `var(--input-background)`
+      // color: `var(--input-text)`,
+      // marginBottom: '16px',
+      // zIndex: `var(--zIndex-overBase)`,
+      // padding: '0 4px'
     }),
-    indicatorsContainer: () => ({
-      position: 'absolute',
-      right: '0px',
-      height: '40px',
-      lineHeight: '36px',
+    indicatorsContainer: (provided: CSSProperties) => ({
+      ...provided,
+      //   position: 'absolute',
+      //   right: '0px',
+      //   height: '40px',
+      //   lineHeight: '36px',
       '@media (min-width: 800px)': {
         height: '30px',
         lineHeight: '28px'
